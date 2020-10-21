62829647d8d16b0161dc5a9d60f880b27040f8b1
charithakishan <59818319+charithakishan@users.noreply.github.com>
2020-10-21 12:43:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,53 +1,27 @@
+const express = require("express");
+const router = express.Router();
 var notes = require("./server")
-const express = require("express")
-const jwt = require("jsonwebtoken")
-const router = express.Router()
 router.post("/", async (req, res) => {
-    var searchid = { email: req.body.email }
-    var search = { email: req.body.email, password: req.body.password };
-    notes.checkEmail(searchid, (err, result) => {
-        if (result) {
-            notes.checkEmail(search, (error, resl) => {
-                if (resl) {
-                    const token = jwt.sign({ searchid }, "my_secret_key1", { expiresIn: '1800s' })
-                    if (token) {
-                        notes.updateToken(searchid, token, (e, r) => {
-                            if (e) {
-                                throw e;
-                            } else {
-                                console.log("token updated to db")
-                                res.send({ email: req.body.email, token: token })
-                            }
-                        })
-                    }
-                } else {
-                    console.log("incorrect password")
-                    res.send("incorrect password")
-                }
-            })
+    var data = req.body;
+    var search = { email: req.body.email }
+    notes.checkEmail(search, function (err, op) {
+        if (op) {
+            console.log("user exists try login")
+            res.send("user exists try login")
         }
         else {
-            console.log("user dosen't exist try registration")
-            res.send("user dosen't exist try registration")
+            notes.registration(data, (error, op1) => {
+                if (error) {
+                    console.log("error while registering user try again")
+                    res.send("error while registering user try again")
+                }
+                else {
+                    console.log("user registration succesful")
+                    res.send("user registration succesful")
+                }
+
+            })
         }
     })
 })
-/*token = (app)=>{
-    
-    app.post("/updatetoken",(req,res)=>{
-        var dbo=con.getDb();
-        dbo.collection("testing").updateOne({email:req.body.email},{$set:{token:req.body.token}},(err,result)=>{
-            if(err)
-            {
-                res.send(err)
-                throw err;
-            }
-            else
-            {
-                console.log("updated"+result)
-                res.send("updated"+result)
-            }
-        })
-    })
-}*/
 module.exports = router
