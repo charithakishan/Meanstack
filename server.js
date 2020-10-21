8dda0ead86726f95b84dd28980eb02a7261da650
charithakishan <59818319+charithakishan@users.noreply.github.com>
2020-10-21 12:43:26 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,70 @@
-const con = require("../connections/index")
-const insert = async (data, collectionname) => {
-    var dbo = con.getDb()
-    var abc = await dbo.collection(collectionname).insert(data)
-    return abc.ops
+const con = require("../../connections/index")
+const checkEmail = (searchid, callback) => {
+  var dbo = con.getDb();
+  dbo.collection("user").findOne(searchid, (err, result) => {
+    if (result) {
+      callback(undefined, result)
+    }
+    else {
+      callback(err, undefined)
+    }
+  })
 }
-const checkEmail = async (email,collectionname)=>{
-    var dbo = con.getDb()
-    var abc = await dbo.collection(collectionname).findOne(email)
-    return abc
+const updateToken = (email, token, cb) => {
+  var dbo = con.getDb();
+  dbo.collection("user").updateOne(email, { $set: { token: token } }, (err, resul) => {
+    if (err)
+      cb(err, undefined)
+    else
+      cb(undefined, resul)
+  })
 }
-module.exports = {insert,checkEmail}
+module.exports = { checkEmail, updateToken }
+
+
+
+// exports.login = (req,res) => {
+
+//         var searchid = {email:req.body.email}
+//         var search ={email:req.body.email,password:req.body.password};
+//         var dbo=con.getDb();
+//   dbo.collection("user").findOne(searchid,function(err,resl){
+//     if(resl)
+//       {
+//         dbo.collection("user").findOne(search,function(err,result){
+//           if(result)
+//             {
+//               const token = jwt.sign({searchid},"my_secret_key1",{expiresIn: '1800s'});
+//               if(token)
+//               {
+//                 dbo.collection("user").updateOne({email:req.body.email},{$set:{token:token}},(err,result)=>{
+//                   if(err)
+//                   {
+//                       //res.send(err)
+//                       throw err;
+//                   }
+//                   else
+//                   {
+//                       console.log("updated"+result+"token:"+token)
+//                       res.send({
+//                         email:req.body.email,
+//                         token:token,
+//                         status:status
+//                     })
+//                   }
+//                 })
+//               }
+
+//             }
+//           else
+//             {
+//               console.log("incorrect password")
+//               res.send("incorrect password")
+//             } });
+//       }
+//     else
+//       {
+//         console.log("user dosen't exist try registration")
+//         res.send("user dosen't exist try registration")
+//       } });
+//       }
