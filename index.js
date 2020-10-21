26ceaa3e10b5a3aa874a54d56d684445f85eb61d
charithakishan <59818319+charithakishan@users.noreply.github.com>
2020-10-21 12:42:08 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
-const express = require("express");
-const router = express.Router();
-const notes = require("./server")
+var notes = require("./server")
+const express = require("express")
+const jwt = require("jsonwebtoken")
+const router = express.Router()
 router.post("/", async (req, res) => {
-    console.log(req.body.email.length)
-    var token1 = req.headers.token1;
-    var mailid = {email:req.body.email};
-    var myobj = [{},{ name: req.body.name, age: req.body.age, gender: req.body.gender, mailid: mailid }]
-    var myobj1 = { currenttown: req.body.currenttown, currentpincode: req.body.currentpincode, mailid: mailid, currentstate: req.body.currentstate }
-    var myobj2 = { mailid: mailid, permanenttown: req.body.permanenttown, permanentpincode: req.body.permanentpincode }
-    const user = await notes.checkEmail(mailid,"user")
-   // if (user) {
-        // if (user.token == token1) {
-            console.log("token verified")
-            var data1 = await notes.insert(myobj, "user")
-            var data2 = await notes.insert(myobj1, "currentaddress")
-            var data3 = await notes.insert(myobj2, "permanentaddress")
-            if ((data1.length > 0)&& (data2.length > 0) && (data3.length > 0)) 
-            {
-                console.log(data1,data2,data3)
-                res.send("data inserted succesfully")
-            }
-        // } else {
-        //     console.log("token doesn't match")
-        //     res.send("token doesn't match")
-        // }
-    // } else {
-    //     console.log("user doesn't exist try login")
-    //     res.send("user doesn't exist try login")
-    // }
+    var searchid = { email: req.body.email }
+    var search = { email: req.body.email, password: req.body.password };
+    notes.checkEmail(searchid, (err, result) => {
+        if (result) {
+            notes.checkEmail(search, (error, resl) => {
+                if (resl) {
+                    const token = jwt.sign({ searchid }, "my_secret_key1", { expiresIn: '1800s' })
+                    if (token) {
+                        notes.updateToken(searchid, token, (e, r) => {
+                            if (e) {
+                                throw e;
+                            } else {
+                                console.log("token updated to db")
+                                res.send({ email: req.body.email, token: token })
+                            }
+                        })
+                    }
+                } else {
+                    console.log("incorrect password")
+                    res.send("incorrect password")
+                }
+            })
+        }
+        else {
+            console.log("user dosen't exist try registration")
+            res.send("user dosen't exist try registration")
+        }
+    })
 })
+/*token = (app)=>{
+    
+    app.post("/updatetoken",(req,res)=>{
+        var dbo=con.getDb();
+        dbo.collection("testing").updateOne({email:req.body.email},{$set:{token:req.body.token}},(err,result)=>{
+            if(err)
+            {
+                res.send(err)
+                throw err;
+            }
+            else
+            {
+                console.log("updated"+result)
+                res.send("updated"+result)
+            }
+        })
+    })
+}*/
 module.exports = router
